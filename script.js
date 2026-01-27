5de1f19efef352c4bd7aaf9edc6b4649bc79cfbc
Kyle Augusto <ks.gusto@gmail.com>
2026-01-27 14:18:29 -0700

    Connect all html actions to script

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll(`[data-number]`);
+const operationButtons = document.querySelectorAll(`[data-operation]`);
+const equalsButton = document.querySelector(`[data-equals]`);
+const deleteButton = document.querySelector(`[data-delete]`);
+const allClearButton = document.querySelector(`[data-all-clear]`);
+const historyDisplayText = document.querySelector(`[data-history-display]`);
+const currentDisplayText = document.querySelector(`[data-current-display]`);
+
